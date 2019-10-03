e4d3944d5cddd121f5bfde8a37ffb38b59cb4877
Andrew Duong <duongsters@gmail.com>
2019-10-03 09:04:16 -0700

    added connection to sql workbench

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,10 @@
 var sql = require("mysql");
 var inquirer = require("inquirer");
+
+var connectiom = sql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "BAMAZON"
+});
