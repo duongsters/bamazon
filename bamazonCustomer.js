0501ecff8a39142027cb8d3b3eec6f467f0c4f49
Andrew Duong <duongsters@gmail.com>
2019-10-03 17:29:21 -0700

    got the application to loop correctly and fixed the top border line of the table to be properly aligned after user runs the application again

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,13 +13,14 @@ var connection = sql.createConnection({
 connection.connect(function(err){
     if(err) throw err;
     console.log("Connected with id: " + connection.threadId);
+    createTable();
 });
 
 var createTable = function() {
     connection.query("SELECT * FROM products", function(err, res) {
         var table = new Table ({
             head: ["item_id", "product_name", "department_name", "price", "stock_quantity"], 
-            colWidths: [15, 20, 20, 20, 20]
+            colWidths: [10, 50, 20, 10, 10]
         });
         // console.log(res);
         for(var j = 0; j <res.length; j++) {
@@ -29,16 +30,17 @@ var createTable = function() {
         };
         
         console.log(table.toString());
+        renderUserChoice();
     });
 };
 
     function renderUserChoice() {
-        createTable();
+        
         inquirer.prompt ([
             {
                 type: "input",
                 name: "userChoice",
-                message: "What is the ID of the item you would like to to purchase?\n\n"
+                message: "\n\n\nWhat is the ID of the item you would like to to purchase?\n"
             },
             {
                 type: "input",
@@ -49,7 +51,8 @@ var createTable = function() {
         .then(function(answers){
             renderInventory(answers.userChoice, answers.itemAmount);
         });
-        
+                
+
     };
     function renderInventory(id, units) {
         connection.query("SELECT * FROM products", function(err, res) {
@@ -69,6 +72,7 @@ var createTable = function() {
             }
         });
     };
+
     function updateInventory(id, startInventory, units, userPurchase, price){
         connection.query("UPDATE products SET ? WHERE ?", 
         [
@@ -83,12 +87,31 @@ var createTable = function() {
             console.log("\nSuccess! Here is your receipt of your order: \n" + units + " " + userPurchase + ".");
             console.log("Total Cost: $" + (units * price) + ".\n");
             
-            renderUserChoice()
-
+            runAgain();
 
         }
         
         )
 }
+            function runAgain() {
+                inquirer.prompt([
+                    {
+                        type: "list",
+                        name: "askAgain",
+                        message: "\n\n\nWould you like to make another purchase from our inventory?\n",
+                        choices: ["Yes", "No"]
+                    }
+                  
+                ])
+                    .then(function(answers){
+                        if(answers.askAgain === "Yes"){
+                            createTable();
+                        }
+                        else {
+                            console.log("Thanks for shopping here at Bamazon!");
+                        }
+                    })
+                }
+            
 
-renderUserChoice();
+
